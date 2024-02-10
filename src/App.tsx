3a3181251f6fe1882a188b9526506507f791e66d
Ekaterina Bondareva <1978231@bk.ru>
2024-02-10 14:44:13 +0600

    list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
 import React from "react";
+import AddTask from "./AddTask";
+import TaskList from "./TaskList";
+import { useState } from "react";
 
 export default function App() {
-  return <div className="App"></div>;
+  const [initialTasks, setInitialTasks] = useState([
+    { id: 0, text: "sport", done: true },
+  ]);
+  const [nextId, setNextId] = useState(1);
+  console.log(initialTasks);
+  function handleAddTask(text: string) {
+    setInitialTasks([...initialTasks, { id: nextId, text: text, done: false }]);
+    setNextId(nextId + 1);
+    console.log(initialTasks);
+    console.log(nextId);
+  }
+
+  return (
+    <>
+      <h1>To-Do List</h1>
+      <AddTask onAddTask={handleAddTask} />
+      <TaskList tasks={initialTasks}/>
+    </>
+  );
 }
---
+++ src/TaskList.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export default function TaskList({ tasks }) {
+  return (
+    <ul>
+      {tasks.map((task) => (
+        <li key={task.id}>
+          <Task task={task} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function Task({ task }) {
+  const [isEditing, setIsEditing] = useState(false);
+  return (
+    <label>
+      <input type="checkbox" checked={task.done} />
+      {task.text}
+    </label>
+  );
+}
