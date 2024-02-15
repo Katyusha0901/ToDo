5e71298b314aadc90629e6d1b7f43db6d41d1354
Ekaterina Bondareva <1978231@bk.ru>
2024-02-15 12:38:43 +0600

    changeName

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,17 @@ import "./App.css";
 import { useState } from "react";
 
 export default function App() {
-  const [initialTasks, setInitialTasks] = useState([
-    { id: 0, text: "sport", done: true },
-  ]);
+  const [tasks, settasks] = useState([{ id: 0, text: "sport", done: true }]);
   const [nextId, setNextId] = useState(1);
 
   function handleAddTask(text: string) {
-    setInitialTasks([...initialTasks, { id: nextId, text: text, done: false }]);
+    settasks([...tasks, { id: nextId, text: text, done: false }]);
     setNextId(nextId + 1);
   }
 
   function handleChangeTask(task: { id: number; text: string; done: boolean }) {
-    setInitialTasks(
-      initialTasks.map((t) => {
+    settasks(
+      tasks.map((t) => {
         if (t.id === task.id) {
           return task;
         } else {
@@ -30,22 +28,22 @@ export default function App() {
   }
 
   function handleDeleteTask(taskId: number) {
-    setInitialTasks(initialTasks.filter((t) => t.id !== taskId));
+    settasks(tasks.filter((t) => t.id !== taskId));
   }
 
   function handleDeleteAll(tasks: []) {
-    const newArray = initialTasks.slice();
+    const newArray = tasks.slice();
     newArray.splice(0, newArray.length);
-    setInitialTasks(newArray);
+    settasks(newArray);
   }
 
   return (
     <div className="allApp">
       <div className="allApp__app">
-        <DeleteAllTask tasks={initialTasks} onDeleteAll={handleDeleteAll} />
+        <DeleteAllTask tasks={tasks} onDeleteAll={handleDeleteAll} />
         <AddTask onAddTask={handleAddTask} />
         <TaskList
-          tasks={initialTasks}
+          tasks={tasks}
           onChangeTask={handleChangeTask}
           onDeleteTask={handleDeleteTask}
         />
