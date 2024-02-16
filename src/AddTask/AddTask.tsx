5c25cd65fde0bd466355f68919837fd38abf1a6c
Ekaterina Bondareva <1978231@bk.ru>
2024-02-16 12:42:36 +0600

    AddTask funciton

--- src/AddTask/AddTask.tsx
+++ src/AddTask/AddTask.tsx
@@ -8,33 +8,34 @@ interface Props {
 export const AddTask: React.FC<Props> = ({ onAddTask }) => {
   const [text, setText] = useState("");
 
+  function AddTask() {
+    setText("");
+    onAddTask(text);
+  }
+
   return (
-    <>
-      <div className="add-task">
-        <input
-          placeholder="Add task"
-          className=" add-task__input-task"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === "Enter" && text.length >= 1) {
-              setText("");
-              onAddTask(text);
-            }
-          }}
-        />
-        <button
-          className=" add-task__button-add"
-          onClick={() => {
-            if (text.length >= 1) {
-              setText("");
-              onAddTask(text);
-            }
-          }}
-        >
-          Add Task
-        </button>
-      </div>
-    </>
+    <div className="add-task">
+      <input
+        placeholder="Add task"
+        className="add-task__input-task"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter" && text.length >= 1) {
+            AddTask();
+          }
+        }}
+      />
+      <button
+        className="add-task__button-add"
+        onClick={() => {
+          if (text.length >= 1) {
+            AddTask();
+          }
+        }}
+      >
+        Add Task
+      </button>
+    </div>
   );
 };
