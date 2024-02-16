2d20d10bbc49d4560b458e557f0c91b49bd0a541
Ekaterina Bondareva <1978231@bk.ru>
2024-02-16 12:39:03 +0600

    return

--- src/DeleteAll/DeleteAllTasks.tsx
+++ src/DeleteAll/DeleteAllTasks.tsx
@@ -3,20 +3,16 @@ import { Task } from "../App";
 
 interface Props {
   tasks: Task[];
-  onDeleteAll: (x: Task[]) => void;
+  onDeleteAll: (value: Task[]) => void;
 }
 
-export const DeleteAllTasks: React.FC<Props> = ({ tasks, onDeleteAll }) => {
-  return (
-    <>
-      <div className="delete-button">
-        <button
-          onClick={() => onDeleteAll(tasks)}
-          className="delete-button__button-delete-all delete-button__delete-all"
-        >
-          Delete All
-        </button>
-      </div>
-    </>
-  );
-};
+export const DeleteAllTasks: React.FC<Props> = ({ tasks, onDeleteAll }) => (
+  <div className="delete-button">
+    <button
+      onClick={() => onDeleteAll(tasks)}
+      className="delete-button__button-delete-all delete-button__delete-all"
+    >
+      Delete All
+    </button>
+  </div>
+);
